cbd068e13a562363d7383a44b3fbc91ebf3aed75
sungeng <sungeng123$>
2021-06-10 11:17:36 +0800

    先保存一下

--- src/main/java/com/bumao/model/yuquesdk/service/HttpClientHolder.java
+++ src/main/java/com/bumao/model/yuquesdk/service/HttpClientHolder.java
@@ -1,33 +1,86 @@
 package com.bumao.model.yuquesdk.service;
 
-import com.bumao.model.yuquesdk.config.YuqueConfig;
+import com.alibaba.fastjson.JSONObject;
 import com.bumao.model.yuquesdk.domain.HttpRespVo;
 import com.bumao.model.yuquesdk.exception.YuqueException;
+import lombok.Data;
 import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
 import org.apache.http.ParseException;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.config.CookieSpecs;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.*;
 
+@Data
 public class HttpClientHolder {
-    protected YuqueConfig config;
-
-    private String yuqueApiBase = null;
+//    private String yuqueApiBase = "https://www.yuque.com/api/v2";
     private String yuqueToken = null;
-    public HttpGet mkGet(String URI){
-        // 创建Get请求
-        HttpGet httpGet = new HttpGet(URI);
-        //设置权限头信息
-        httpGet.setHeader("X-Auth-Token","EwIAnaYbkUp8hwfOumMEee3swog5lFOOlNLwvaon");
+
+    void HttpClientHolder(String Token){
+        this.yuqueToken = Token;
+    }
+
+    /**
+     * 造GET
+     * @param URI
+     * @return
+     */
+    public HttpGet buildHttpGet(String URI) throws YuqueException {
+        return this.buildHttpGet(URI,null);
+    }
+    public HttpGet buildHttpGet(String URI, Map<String, String> para) throws YuqueException {
+        HttpGet httpGet = null;
+        try {
+            URIBuilder builder = new URIBuilder(URI);
+            if (para != null) {
+                Set<String> set = para.keySet();
+                for (String key : set) {
+                    builder.setParameter(key, para.get(key));
+                }
+            }
+            httpGet = new HttpGet(builder.build());
+        }catch (URISyntaxException e){
+            throw new YuqueException(e.getMessage(),e);
+        }
         return httpGet;
+    }
+
+    /**
+     * 造POST
+     * @param URI
+     * @param map
+     * @return
+     * @throws YuqueException
+     */
+    public HttpPost buildHttpPost(String URI,Map<String,String> map) throws YuqueException {
+        HttpPost httpPost = null;
+        try {
+            URIBuilder builder = new URIBuilder(URI);
+            if(map!=null) {
+                List<NameValuePair> pairs = new ArrayList<>();
+                for (Map.Entry<String, String> entry : map.entrySet()) {
+                    pairs.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
+                }
+                builder.setParameters(pairs);
+            }
+            httpPost = new HttpPost(builder.build());
+        }catch (URISyntaxException e){
+            throw new YuqueException(e.getMessage(),e);
+        }
+        return httpPost;
     }
     /**
      * 发送http请求
@@ -35,6 +88,8 @@ public class HttpClientHolder {
      * @return
      */
     public HttpRespVo doRequest(HttpRequestBase httpRequestBase) throws YuqueException {
+        //设置权限头信息
+        httpRequestBase.setHeader("X-Auth-Token",this.yuqueToken);
         //解决cookies报错问题
         RequestConfig defaultConfig = RequestConfig.custom().setCookieSpec(CookieSpecs.STANDARD).build();
         httpRequestBase.setConfig(defaultConfig);
@@ -56,12 +111,34 @@ public class HttpClientHolder {
                 vo.setHttpContent(EntityUtils.toString(responseEntity));
             }
             if( !vo.getHttpCode().equals(200) ){
-                new YuqueException.Builder()
-                        .setHttpCode(vo.getHttpCode())
-                        .setHttpStatusLine(vo.getHttpStatusLine())
-                        .setOrigContent(vo.getHttpContent())
-                        .setCustomErrorMsg("返回代码非200")
-                        .build();
+                //处理400问题
+                if(vo.getHttpCode().equals(400)){
+                    Map<String,String> digMap = dig400(vo.getHttpContent());
+                    if(digMap==null){
+                        throw new YuqueException(
+                            vo.getHttpCode(),
+                            vo.getHttpStatusLine(),
+                            vo.getHttpContent(),
+                            "unknow Message",
+                            "unknow Code"
+                        );
+                    }else{
+                        throw new YuqueException(
+                                vo.getHttpCode(),
+                                vo.getHttpStatusLine(),
+                                vo.getHttpContent(),
+                                digMap.get("message"),
+                                digMap.get("code")
+                        );
+                    }
+                }else {
+                    throw new YuqueException.Builder()
+                            .setHttpCode(vo.getHttpCode())
+                            .setHttpStatusLine(vo.getHttpStatusLine())
+                            .setOrigContent(vo.getHttpContent())
+                            .setCustomErrorMsg("返回代码非200")
+                            .build();
+                }
             }
         } catch (ClientProtocolException e) {
             throw new YuqueException(e.getMessage(),e);
@@ -83,5 +160,17 @@ public class HttpClientHolder {
             }
         }
         return vo;
+    }
+    private Map<String,String> dig400(String content){
+        Map<String,String> map = new HashMap<>();
+        try {
+            JSONObject json = JSONObject.parseObject(content);
+            map.put("message",json.getString("message"));
+            map.put("code",json.getString("code"));
+            return map;
+        }catch (Exception e){
+            return null;
+        }
+
     }
 }
